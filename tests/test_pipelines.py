2b1f34d2f3bb1cb42bcf99fc92f6192cd518e302
soderc <christoffer.soderberg@ons.gov.uk>
2026-09-08 17:46:51 +0100

    Organise test_pipelines into a class

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -9,43 +9,42 @@ from pystagegate.pipelines import (
 
 
 @pytest.mark.parametrize("config", ["test_config", "test_config_path"])
-def test_prov_fin_dict(request, config, test_config):
-    output = prov_fin_main(request.getfixturevalue(config))
+class TestPipelines:
+    def test_prov_fin_dict(self, request, config):
+        output = prov_fin_main(request.getfixturevalue(config))
 
-    expected_output = pd.read_csv(
-        os.path.join("tests/data/prov_fin/prov_fin_output.csv")
-    )
+        expected_output = pd.read_pickle("tests/data/prov_fin/prov_fin_output.pkl")
 
-    pd.testing.assert_frame_equal(output, expected_output)
+        pd.testing.assert_frame_equal(output, expected_output)
 
+    def test_sex_ratio_national_profile_dict(self, request, config):
+        sq_diff, year_agg, year_agg_adjusted = sex_ratio_national_profile(
+            request.getfixturevalue(config)
+        )
 
-@pytest.mark.parametrize("config", ["test_config", "test_config_path"])
-def test_sex_ratio_national_profile_dict(request, config):
-    sq_diff, year_agg, year_agg_adjusted = sex_ratio_national_profile(
-        request.getfixturevalue(config)
-    )
+        expected_sq_diff = pd.read_pickle(
+            "tests/data/sex_ratio/provisional_final_ssq.pkl"
+        )
+        expected_year_agg = pd.read_pickle("tests/data/sex_ratio/year_agg_ssq.pkl")
+        expected_year_agg_adjusted = pd.read_pickle(
+            "tests/data/sex_ratio/year_agg_adjusted_ssq.pkl"
+        )
 
-    expected_sq_diff = pd.read_csv("tests/data/sex_ratio/provisional_final_ssq.csv")
-    expected_year_agg = pd.read_csv("tests/data/sex_ratio/year_agg_ssq.csv")
-    expected_year_agg_adjusted = pd.read_csv(
-        "tests/data/sex_ratio/year_agg_adjusted_ssq.csv"
-    )
+        pd.testing.assert_frame_equal(sq_diff, expected_sq_diff)
+        pd.testing.assert_frame_equal(year_agg, expected_year_agg)
+        pd.testing.assert_frame_equal(year_agg_adjusted, expected_year_agg_adjusted)
 
-    pd.testing.assert_frame_equal(sq_diff, expected_sq_diff)
-    pd.testing.assert_frame_equal(year_agg, expected_year_agg)
-    pd.testing.assert_frame_equal(year_agg_adjusted, expected_year_agg_adjusted)
+    def test_sex_ratio_main(self, request, config):
+        sr, sr_national, sr_merged_ssq = sex_ratio_main(request.getfixturevalue(config))
 
+        expected_sr = pd.read_pickle("tests/data/sex_ratio/sex_ratio_recoded.pkl")
+        expected_sr_national = pd.read_pickle(
+            "tests/data/sex_ratio/sex_ratio_national.pkl"
+        )
+        expected_sr_merged_ssq = pd.read_pickle(
+            "tests/data/sex_ratio/sex_ratio_ssq.pkl"
+        )
 
-@pytest.mark.parametrize("config", ["test_config", "test_config_path"])
-def test_sex_ratio_main(request, config):
-    sr, sr_national, sr_merged_ssq = sex_ratio_main(request.getfixturevalue(config))
-
-    expected_sr = pd.read_csv("tests/data/sex_ratio/sex_ratio_recoded.csv")
-    expected_sr_national = pd.read_csv("tests/data/sex_ratio/sex_ratio_national.csv")
-    expected_sr_merged_ssq = pd.read_csv("tests/data/sex_ratio/sex_ratio_ssq.csv")
-
-    print("\n\n", sr, expected_sr, "\n\n")
-
-    pd.testing.assert_frame_equal(sr, expected_sr)
-    pd.testing.assert_frame_equal(sr_national, expected_sr_national)
-    pd.testing.assert_frame_equal(sr_merged_ssq, expected_sr_merged_ssq)
+        pd.testing.assert_frame_equal(sr, expected_sr)
+        pd.testing.assert_frame_equal(sr_national, expected_sr_national)
+        pd.testing.assert_frame_equal(sr_merged_ssq, expected_sr_merged_ssq)
